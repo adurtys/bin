019c94fd4c9c946efe5cd37e203436ef4651fd57
adurtys <adurtys@gmail.com>
2018-06-21 16:34:58 -0400

    modified python pipeline

--- pipeline_v1.py
+++ pipeline_v1.py
@@ -4,3 +4,23 @@
 filenames = ["ATAC_67S_reseq1_1.fq.gz", "ATAC_HMTB067S_1.fq.gz", "ATAC_67S_reseq1_2.fq.gz", "ATAC_HMTB067S_2.fq.gz", "ATAC_67V_reseq1_1.fq.gz", "ATAC_HMTB067V_1.fq.gz", "ATAC_67V_reseq1_2.fq.gz", "ATAC_HMTB067V_2.fq.gz"]
 
 # for name in filenames:
+	# chmod +x filename
+	
+	# perform quality control analysis
+	# fastqc ./filename
+	
+	# perform adaptor removal
+
+	# align reads to hg19 genome
+
+	# perform quality control of alignment reads
+	# remove mitochondrial reads
+	# remove PCR duplicates
+
+	# peak calling for regions of genomic enrichment
+
+	# visualization
+
+	# compare peaks with other peak files (BEDTools)
+		# annotation of peaks
+		# motif finding
