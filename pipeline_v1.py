b369427ca7c51893425f9220afd148af37943c7a
adurtys <adurtys@gmail.com>
2018-06-25 13:13:31 -0400

    modified pipelines

--- pipeline_v1.py
+++ pipeline_v1.py
@@ -12,7 +12,7 @@ filenames = ["ATAC_67S_reseq1_1.fq.gz", "ATAC_HMTB067S_1.fq.gz", "ATAC_67S_reseq
 		# perform quality control analysis
 		# fastqc ./filename
 	
-		# perform adaptor removal with trimmomatic
+		# perform adapter removal with trimmomatic
 		# bsub -q voight_normal -o trimmomaticTest.out "java -jar /appl/Trimmomatic-0.36/trimmomatic-0.36.jar 
 			# PE forward_file.fq.gz (ex: ATAC_67S_reseq1_1.fq.gz) reverse_file.fq.gz (ex: ATAC_67S_reseq1_2.fq.gz) 
 			# forward_paired_out.fq.gz (ex: out_67S_1P.fq.gz) forward_unpaired_out.fq.gz (out_67S_1U.fq.gz) 
@@ -24,15 +24,23 @@ filenames = ["ATAC_67S_reseq1_1.fq.gz", "ATAC_HMTB067S_1.fq.gz", "ATAC_67S_reseq
 		# chmod +x reverse_paired_out.fq.gz
 		# STEP 1: generate alignments in suffix array coordinate
 		# bsub -q voight_normal -o bwaTest1.out "bwa aln ../bwa_index/hg19.fa ./forward_paired_out.fq.gz 
-			# (out_67S_1P.fq.gz) > filename1.sai (ex: 67S_1P.sai)"
+			# (ex: out_67S_1P.fq.gz) > filename1.sai (ex: 67S_1P.sai)"
 		# bsub -q voight_normal -o bwaTest2.out "bwa aln ../bwa_index/hg19.fa ./reverse_paired_out.fq.gz 
-			# (out_67S_2P.fq.gz) > filename2.sai (ex: 67S_2P.sai)"
+			# (ex: out_67S_2P.fq.gz) > filename2.sai (ex: 67S_2P.sai)"
 
-		# STEP 2: generate alignments in the SAM format:
+		# STEP 2: generate alignments in the SAM format
 		# chmod +x filename1.sai
 		# chmod +x filename2.sai
-		# bsub -q voight_normal -o bwa_sam_align_test1.out "bwa sampe ../bwa_index/hg19.fa ./filename1.sai (ex: 67S_1P.sai)"
-		# bsub -q voight_normal -o bwa_sam_align_test2.out "bwa sampe ../bwa_index/hg19.fa ./filename2.sai (ex: 67S_2P.sai)"
+		# bsub -q voight_normal -o bwa_sam_align_test.out "bwa sampe ../bwa_index/hg19.fa ./filename1.sai 
+			# (ex: 67S_1P.sai) ./filename2.sai (ex: 67S_2P.sai) forward_paired_out.fq.gz (ex: out_67S_1P.fq.gz) 
+			# reverse_paired_out.fq.gz (ex: out_67S_2P.fq.gz) > aln.sam"
+
+		# STEP 3: get multiple hits
+		# bsub -q voight_normal -o bwa_hits_test1.out "bwa samse -n 100 ../bwa_index/hg19.fa ./filename1.sai (ex: 67S_1P.sai) 
+			# ./forward_paired_out.fq.gz (ex: out_67S_1P.fq.gz)"
+		# bsub -q voight_normal -o bwa_hits_test2.out "bwa samse -n 100 ../bwa_index/hg19.fa ./filename2.sai (ex: 67S_2P.sai) 
+			# ./reverse_paired_out.fq.gz (ex: out_67S_2P.fq.gz)"
+
 
 	# perform quality control of alignment reads
 	# remove mitochondrial reads
